c78bfcfde8c953aaf1fb20188414b77ff3577ebe
sagar sharma <153342678+Sagarsharmaa1@users.noreply.github.com>
2026-08-25 00:01:16 +0530

    Time: 39 ms (5.73%) | Memory: 47.1 MB (5.11%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,43 +1,33 @@
-
 class Solution {
-    public int[][][] dp;
-
-    public int f(int i, int m, int turn, int[] piles) {
-        int n = piles.length;
-        if (i == n) {
-            return 0;
-        }
-        if (dp[i][m][turn] != -1) return dp[i][m][turn];
-        
-        int alice;
-        if (turn == 1) { 
-            alice = 0;
-            int sum = 0;
-            for (int ind = i; ind < i + 2 * m; ind++) {
-                if (ind == n) break;
-                sum += piles[ind];
-                int x = ind - i + 1;
-                alice = Math.max(alice, sum + f(ind + 1, Math.max(x, m), 0, piles));
-            }
-        } else { 
-            alice = Integer.MAX_VALUE;
-            for (int ind = i; ind < i + 2 * m; ind++) {
-                if (ind == n) break;
-                int x = ind - i + 1;
-                alice = Math.min(alice, f(ind + 1, Math.max(x, m), 1, piles));
-            }
-        }
-        return dp[i][m][turn] = alice;
-    }
-
     public int stoneGameII(int[] piles) {
         int n = piles.length;
-        dp = new int[n][n + 1][2];
-        for (int[][] arr2D : dp) {
-            for (int[] arr1D : arr2D) {
-                Arrays.fill(arr1D, -1);
+        int[][][] dp = new int[n + 1][n + 1][2];
+        
+        for(int i = n - 1; i >= 0; i--) {
+            for(int m = 0; m <= n; m++) {
+                for(int turn = 0; turn <= 1; turn++) {
+                    int alice;
+                    if(turn == 1) {
+                        alice = 0;
+                        int sum = 0;
+                        for(int ind = i; ind < i + 2 * m; ind++) {
+                            if(ind == n) break;
+                            sum += piles[ind];
+                            int x = ind - i + 1;
+                            alice = Math.max(alice, sum + dp[ind + 1][Math.max(x, m)][0]);
+                        }
+                    } else { 
+                        alice = Integer.MAX_VALUE;
+                        for(int ind = i; ind < i + 2 * m; ind++) {
+                            if(ind == n) break;
+                            int x = ind - i + 1;
+                            alice = Math.min(alice, dp[ind + 1][Math.max(x, m)][1]);
+                        }
+                    }
+                    dp[i][m][turn] = alice;
+                }
             }
         }
-        return f(0, 1, 1, piles);
+        return dp[0][1][1];
     }
 }
